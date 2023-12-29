0e561feba124fcfd0f1db54c1eacf30ad264b2e8
Jhordan S <jhordan.kendy@outlook.com>
2023-12-29 11:34:03 -0300

    css da aplicação

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  function handleSearch() {
+    alert("valor " + input)
+  }
+
   return (
     <div className="container">
-     <h1 className="title">Buscador de CEP</h1>
+     <h1 className="title">Buscador CEP</h1>
 
-     <div className="containerInput">
+     <div className="container-input">
       <input 
       type="text"
-      placeholder="Digite seu CEP"/>
+      placeholder="Digite seu CEP"
+      value={input}
+      onChange={(e)=> setInput(e.target.value)}
+      />
 
-      <button className="buttonSearch">
+      <button className="buttonSearch" onClick={handleSearch}>
         <FiSearch size={25} color="#FFF"/>
       </button>
      </div>
